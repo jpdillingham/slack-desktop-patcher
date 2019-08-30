a4002abd8dd6e6663b5cb8e7d5c727066705649f
JP Dillingham <JP.Dillingham@rackspace.com>
2019-08-30 11:39:00 -0500

    add fetch of source, listing of app versions

--- src/sdp.go
+++ src/sdp.go
@@ -1,31 +1,57 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"regexp"
 	"runtime"
 )
 
+var srcURL = "https://raw.githubusercontent.com/jpdillingham/slack-desktop-dark-theme/master/loader.js"
+var space = func() { fmt.Println() }
+
 func main() {
+	// parse args
+	srcURLInputPtr := flag.String("src", "", "The source of the patch code")
+	flag.Parse()
+
+	space()
+
+	// figure out where Slack should be installed (OS specific) and make sure the directory exists
 	path := getSlackDir()
 
-	fmt.Println(fmt.Sprintf("Slack directory: %s", path))
-
-	files, err := ioutil.ReadDir(path)
-	if err != nil {
-		panic("error reading Slack directory.")
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic(fmt.Sprintf("Slack directory was expected to be %s, but was not found.", path))
+	} else {
+		fmt.Println(fmt.Sprintf("Slack found in %s.", path))
 	}
 
-	for _, f := range files {
-		name := f.Name()
-		isAppDir, _ := regexp.MatchString("^app-((\\d+\\.?){3})$", name)
+	space()
 
-		if f.IsDir() && isAppDir {
-			fmt.Println(fmt.Sprintf("Detected Slack version: %s", name))
-		}
+	// list all of the installed Slack versions by matching subdirectories against 'app-N.N.N'
+	fmt.Println("Installed Slack versions:")
+
+	installedVersions := getInstalledSlackVersions(path)
+
+	for i, f := range installedVersions {
+		fmt.Printf("%d) %s\n", i, f)
 	}
+
+	fmt.Printf("\nPlease select the version to patch: ")
+
+	if *srcURLInputPtr != "" {
+		srcURL = *srcURLInputPtr
+		fmt.Print(fmt.Sprintf("Fetching source from %s...", srcURL))
+	} else {
+		fmt.Print(fmt.Sprintf("Fetching source from default location %s...", srcURL))
+	}
+
+	getPatchSrc(srcURL)
+
+	fmt.Print(" done.")
 }
 
 func getSlackDir() (path string) {
@@ -37,4 +63,41 @@ func getSlackDir() (path string) {
 	default:
 		panic("unsupported OS.")
 	}
+}
+
+func getInstalledSlackVersions(path string) (versions []string) {
+	var installedVersions []string
+	installedVersions = make([]string, 0)
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic("error listing files in Slack directory.  Try again with escalated privileges?")
+	}
+
+	for _, f := range files {
+		name := f.Name()
+		isAppDir, _ := regexp.MatchString("^app-((\\d+\\.?){3})$", name)
+
+		if f.IsDir() && isAppDir {
+			installedVersions = append(installedVersions, f.Name())
+		}
+	}
+
+	return installedVersions
+}
+
+func getPatchSrc(srcUrl string) (src string) {
+	resp, err := http.Get(srcUrl)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(body)
 }
